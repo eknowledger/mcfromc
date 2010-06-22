68a9d39344c0a64b9c5fc949abadeb51a50e603a
Avishay.Ben.Shabtai <Avishay.Ben.Shabtai@46565ccc-2a3b-0f42-2bc5-e45c9856c86c>
2010-06-22 07:10:52 +0000

    Added Initial files

---
+++ trunk/Input/test.c
@@ -0,0 +1,32 @@
+int foo(int x)
+{
+	return x*x;
+}
+
+int main()
+{
+	int i=0, z=5, k=3;
+	int j = z = i;
+	int ji=18;
+
+    i=i*5+7;
+ 	
+	do {
+		while (k > 0) {
+			for (z = 5; z < 100; ++z) {
+				if(x<5)
+				{
+					z=0;
+					k=i*5;
+				}
+				else
+				{
+					k--;
+					j=0;
+					ji++ = (++i)*5;
+				}
+			}
+		}
+	}
+	while(1);
+}
---
+++ trunk/SyntaxParser/SyntaxTree.h
@@ -0,0 +1,66 @@
+#pragma once
+
+typedef enum {
+	CONDITION_IF, 
+	CONDITION_IF_ELSE, 
+	STATEMENT, 
+	COMPOUND_STATEMENT,
+	STATEMENT_LIST,
+	EXPRESSION_STATEMENT,
+	SELECTION_STATEMENT,
+	EXPRESSION,
+	CONDITIONAL_EXPRESSION,
+	ASSIGNMENT_EXPR,
+	LOGICAL_OR_EXPR,
+    LOGICAL_AND_EXPR,
+	EQUAL_EXPR,
+	NOT_EQUAL_EXPR,
+	LESS_THAN_EXPR,
+	GREATER_THAN_EXPR,
+	LESS_EQ_THAN_EXPR,
+	GREATER_EQ_THAN_EXPR,
+	AND_EXPR,
+	ADD_EXPR,
+	SUB_EXPR,
+	MULT_EXPR,
+	DIV_EXPR,
+	MOD_EXPR,
+	PRE_INCREMENT_UNARY_EXPR,
+	PRE_DECREMENT_UNARY_EXPR,
+	POST_INCREMENT_EXPR,
+	POST_DECREMENT_EXPR,
+	PREFIX_OP_UNARY_EXPR,
+	PREFIX_PLUS,
+	PREFIX_MINUS,
+	PREFIX_NOT,
+	BLOCK,
+	NONE,
+	ID,
+	CONST_TYPE,
+	STR_LITERAL,
+	DECLARATION,
+	INIT_DECLARATOR_LIST,
+	TYPE_SPECIFIER,
+	PARAM_LIST,
+	PARAM_DECLARATION,
+	DECLARATION_LIST,
+	TRANSLATION_UNIT,
+	WHILE_LOOP,
+	DO_WHILE_LOOP,
+	FOR_LOOP,
+	NOP} nodeType;
+
+typedef struct tagNode{
+	nodeType m_type;
+	unsigned int m_childCount;
+	unsigned int m_size;
+	char* m_name;
+	int   m_constVal;
+	struct tagNode **m_childs;
+} Node;
+
+Node *createNode(nodeType type);
+void appendChild(Node *parent,Node *child);
+Node *getChildAt(Node *parent , unsigned int idx);
+unsigned int getChildsCount(Node *parent);
+void print(Node *root, unsigned int level);
