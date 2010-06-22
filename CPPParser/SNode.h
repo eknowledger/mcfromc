31b6f6d106b14372aa66cf900d4a947580dca691
Avishay.Ben.Shabtai <Avishay.Ben.Shabtai@46565ccc-2a3b-0f42-2bc5-e45c9856c86c>
2010-06-22 15:12:58 +0000

    a) Changed the Build to be more generic by using CommonProps b) removed generated file from SVN

--- CPPParser/SNode.h
+++ CPPParser/SNode.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <vector>
-#include <SyntaxParserInterface.h>
+#include "SyntaxParserInterface.h"
 
 class SNode
 {
--- CPPParser/main.cpp
+++ CPPParser/main.cpp
@@ -1,4 +1,4 @@
-#include <SyntaxParserInterface.h>
+#include "SyntaxParserInterface.h"
 #include "SNode.h"
 
 int main(int argc, char** argv)
