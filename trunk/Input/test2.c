7baad3566de0eece3dbd66b3a7c8cdf2dbb3e48b
man701 <man701@46565ccc-2a3b-0f42-2bc5-e45c9856c86c>
2010-06-29 21:52:30 +0000

    Added another test program

---
+++ trunk/Input/test2.c
@@ -0,0 +1,43 @@
+int foo()
+{
+	return 174;
+}
+
+int main()
+{
+	int i=0, z=5, k=3;
+	int j = z = i;
+	int ji=18;
+
+    i=i*5+7;
+ 	
+	do {
+		if (i > 10) {
+			while (k > 0) {
+				k = z/--i;
+				ji = k + 5;
+				for (z = 5; z < 100; ++z) {
+					k = z/2;
+					i = k-z;
+					if(x<5)
+					{
+						z = x;
+						k=i*5;
+					}
+					else
+					{
+						if (x>5) {
+							k--;
+						}
+						j=0;
+						ji++ = (++i)*5;
+					}
+					k = z*2;
+					i = k+z;
+				}
+				x = 13*ji - k;
+			}
+		}
+	}
+	while(1);
+}
