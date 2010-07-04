2d1ffc99e55b94e72a3761192e47c6c2b5e51b86
man701 <man701@46565ccc-2a3b-0f42-2bc5-e45c9856c86c>
2010-07-04 20:57:23 +0000

    Enhancements to the CFGViewer app.

--- trunk/CFGViewer/CFGViewerForm.cs
+++ trunk/CFGViewer/CFGViewerForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO;
 using ParserDotNetBridge;
+using System.Threading;
 
 namespace CFGViewer
 {
@@ -16,11 +17,13 @@ namespace CFGViewer
         public CFGViewerForm()
         {
             InitializeComponent();
-            GraphPictureBox.Image = Image.FromFile("Untitled1.png");
+            codeTextBox.BackColor = Color.White;
+            codeTextBox.ForeColor = codeTextBox.ReadOnly ? Color.Gray : Color.Black;
+            //GraphPictureBox.Image = Image.FromFile("Untitled1.png");
             m_app = new CFGViewerApp();
-            codeTextBox.Text = m_app.readFlowPoints("test2.c");
-            m_app.readDotSpec("out.dot");
-            GraphPictureBox.Size = new Size(m_app.ImageWidth, m_app.ImageHeight);
+            //codeTextBox.Text = m_app.readFlowPoints("test2.c");
+            //m_app.readDotSpec("out.dot");
+            //GraphPictureBox.Size = new Size(m_app.ImageWidth, m_app.ImageHeight);
         }
 
         private void GraphPictureBox_MouseClick(object sender, MouseEventArgs e)
@@ -76,5 +79,73 @@ namespace CFGViewer
         }
 
         private CFGViewerApp m_app;
+        private const string TEMP_C_FILE_PATH = "temp.c";
+
+        private void SetMessage(string text)
+        {
+            CFGProgressBar.CreateGraphics().DrawString(
+                text, 
+                new Font("Arial", (float)8.25, FontStyle.Regular), 
+                Brushes.Black, 
+                new PointF(CFGProgressBar.Width / 8 - 10, CFGProgressBar.Height / 2 - 7));
+            Thread.Sleep(200);
+        }
+        private void OpenFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Multiselect = false;
+            dlg.Filter = ".c files|*.c|All files|*.*";
+            DialogResult res = dlg.ShowDialog(this);
+            if (res == DialogResult.OK) 
+            {
+                this.Text = "Control Flow Graph Viewer" + " - " + dlg.FileName;
+                codeTextBox.Text = m_app.readFlowPoints(dlg.FileName);
+            }
+            
+        }
+
+        private void EditCodeButton_Click(object sender, EventArgs e)
+        {
+            codeTextBox.ReadOnly = !codeTextBox.ReadOnly;
+            codeTextBox.BackColor = Color.White;
+            codeTextBox.ForeColor = codeTextBox.ReadOnly ? Color.Gray : Color.Black;
+        }
+
+        private void GenerateCFGButton_Click(object sender, EventArgs e)
+        {
+            bool fileRead = false;
+            CFGProgressBar.Show();
+            CFGProgressBar.Value = 0;
+            CFGProgressBar.Value += 15;
+            SetMessage("Writing temporary code file '" + TEMP_C_FILE_PATH + "'");
+            try
+            {        
+                using (StreamWriter writer = new StreamWriter(TEMP_C_FILE_PATH, false)) {
+                    writer.Write(codeTextBox.Text);
+                    writer.Flush();
+                    writer.Close();
+                }
+                CFGProgressBar.Value += 15;
+                //SetMessage("Finished writing temporary code file '" + TEMP_C_FILE_PATH + "'");
+                SetMessage("Generating flow points");
+                m_app.readFlowPoints(TEMP_C_FILE_PATH);
+                CFGProgressBar.Value += 40;
+                //SetMessage("Finished generating flow points");
+                fileRead = true;
+                SetMessage("Cleaning up");
+                 File.Delete(TEMP_C_FILE_PATH);
+                CFGProgressBar.Value += 30;
+                SetMessage("");
+            }
+            catch (System.Exception ex)
+            {
+                if (!fileRead)
+                {
+                }
+            	
+            }
+            CFGProgressBar.Value += 10;
+            CFGProgressBar.Hide();
+        }
     }
 }
