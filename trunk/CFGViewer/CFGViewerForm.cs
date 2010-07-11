c072665d92e9b3a14fd358e528789c780ed1802f
man701@gmail.com <man701@gmail.com@46565ccc-2a3b-0f42-2bc5-e45c9856c86c>
2010-07-11 05:10:50 +0000

    CFGViewer: moved logic of graph mouse click interpretation to CFGViewerApp.

--- trunk/CFGViewer/CFGViewerForm.cs
+++ trunk/CFGViewer/CFGViewerForm.cs
@@ -26,55 +26,17 @@ namespace CFGViewer
 
         private void GraphPictureBox_MouseClick(object sender, MouseEventArgs e)
         {
-            int minDistance = 10000000;
-            Point closestPoint = new Point(-1,-1);
-            Point imgSize = new Point(GraphPictureBox.Image.Size);
-            Point cntrlSize = new Point(GraphPictureBox.Size);
+            int start, count;
+            m_app.TranslateGraphClickToCodeRange(e.Location,
+                                GraphPictureBox.Image.Size,
+                                GraphPictureBox.Size,
+                                out start, out count);
 
-            int xClick = e.X;
-            int yClick = cntrlSize.Y - e.Y;
-            foreach (Point p in m_app.Locations)
-            {
-                int diffX = p.X-xClick; 
-                int diffY = p.Y-yClick;
-                int dist =  diffX*diffX + diffY*diffY;
-                if (dist < minDistance)
-                {
-                    minDistance = dist;
-                    closestPoint = p;
-                }
-            }
-
-            if (closestPoint != null) {
-                if (m_app.FlowPoint.ContainsKey(m_app.FlowPointName[closestPoint]))
-                {
-                    VisualFlowPoint fp = m_app.FlowPoint[m_app.FlowPointName[closestPoint]];
-                    if (fp != null)
-                    {
-                        int rowNum = 0;
-                        int charCount = 0;
-                        int charStart = 0;
-                        for (int i = 0; i < codeTextBox.Lines.Length; i++)
-                        {
-                            if (i == fp.Row)
-                            {
-                                charStart = charCount;
-                                charCount = codeTextBox.Lines[i].Length;  
-                                break;
-                            }
-
-                            charCount+=codeTextBox.Lines[i].Length + 2;
-
-                        }
-
-                        //codeTextBox.Lines = lines;
-                        //int index = m_app.findOpeningParentheses(codeTextBox.Text, charStart);
-                        codeTextBox.SelectionStart = charStart;
-                        codeTextBox.SelectionLength = charCount;
-                    }
-                }
-            }
-        }
+            //codeTextBox.Lines = lines;
+            //int index = m_app.findOpeningParentheses(codeTextBox.Text, charStart);
+            codeTextBox.SelectionStart = start;
+            codeTextBox.SelectionLength = count;
+          }
 
         private CFGViewerApp m_app;
 
